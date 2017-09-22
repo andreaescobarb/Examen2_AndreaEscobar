4d221f5b553ad1bf395d8b370a962b091cf13ce7
Andrea <andreajaninaescobar@gmail.com>
2017-09-22 14:17:52 -0600

    Clases

--- Examen2_AndreaEscobar_Labp2/src/examen2_andreaescobar_labp2/Admin.java
+++ Examen2_AndreaEscobar_Labp2/src/examen2_andreaescobar_labp2/Admin.java
@@ -9,13 +9,6 @@ package examen2_andreaescobar_labp2;
  *
  * @author andre
  */
-public class Examen2_AndreaEscobar_Labp2 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Admin {
     
 }
